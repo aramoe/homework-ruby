6691aad122b80f8847db6417f1e5763bb9a5b947
Moe Arashiba <moe.arashiba@gmail.com>
2020-10-31 10:29:38 +0900

    順列組み合わせのメソッドを利用

---
+++ send_more_money/smm3.rb
@@ -0,0 +1,36 @@
+# 順列組み合わせのメソッドを使った
+
+def put_answer(s, e, n, d, m, o, r, y)
+  puts "s=#{s} e=#{e} n=#{n} d=#{d} m=#{m} o=#{o} r=#{r} y=#{y}"
+  puts ""
+  puts "  #{s}#{e}#{n}#{d}"
+  puts "+ #{m}#{o}#{r}#{e}"
+  puts "------"
+  puts " #{m}#{o}#{n}#{e}#{y}"
+  puts ""
+end
+
+def assign_ok(s, e, n, d, m, o, r, y)
+  if s == 0 || m == 0
+    return false
+  end
+
+  return true
+end
+
+def calc_ok(s, e, n, d, m, o, r, y)
+  send = s * 1000  +  e * 100  +  n * 10  +  d
+  more = m * 1000  +  o * 100  +  r * 10  +  e
+  money = m * 10000  +  o * 1000  +  n * 100  +  e * 10  +  y
+  return send + more == money
+end
+
+(0..9).to_a.combination(8) do |c|
+  c.permutation do |p|
+    if assign_ok(*p)
+      if calc_ok(*p)
+        put_answer(*p)
+      end
+    end
+  end
+end
